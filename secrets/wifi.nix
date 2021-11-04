24b8afd5b5b485b8cc51d8e8e81263413581e557
DasenB <14915246+DasenB@users.noreply.github.com>
2021-11-04 09:41:41 +0100

    remove autoconnect configuration

--- secrets/wifi.nix
+++ secrets/wifi.nix
@@ -3,7 +3,7 @@
 {
     networking.wireless.networks.NETWORK = {
       psk = "";
-      autoconnectEnable = true;
-      autoconnectPriority = 1;
+      # autoconnectEnable = true;
+      # autoconnectPriority = 1;
     };
 }
