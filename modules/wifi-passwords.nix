87039a6d7bec1efdc8107f520bc0f9534a2b4f75
DasenB <14915246+DasenB@users.noreply.github.com>
2021-11-04 10:33:57 +0100

    change options definition

--- modules/wifi-passwords.nix
+++ modules/wifi-passwords.nix
@@ -29,37 +29,39 @@ let
     };
   };
 
-  keyFiles = mapAttrs' createWifi config.networking.wireless.networks;
+  keyFiles = mapAttrs' createWifi config.networking.networkmanager.networks;
 in {
 
-  options.networking.networkmanager.networks = mkOption {
-    type = types.attrsOf (types.submodule {
-      options = {
-        psk = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-                The network's pre-shared key in plaintext defaulting
-                to being a network without any authentication.
-                Be aware that these will be written to the nix store
-                in plaintext!
-              '';
-        };
-        autoconnect = mkOption {
-          type = types.bool;
-          default = true;
-          description = ''
-                Set this to <literal>false</literal> to prevent NetworkManager
-                from automaticly connecting to this network.
-              '';
-        };
-        priority = mkOption {
-          type = types.int;
-          default = 0;
-          description = ''
-                When connecting automaticly networks with a higher priority are prevered 
-                over networks with lower priority.
-              '';
+  options = {
+    networking.networkmanager.networks = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          psk = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = ''
+                  The network's pre-shared key in plaintext defaulting
+                  to being a network without any authentication.
+                  Be aware that these will be written to the nix store
+                  in plaintext!
+                '';
+          };
+          autoconnect = mkOption {
+            type = types.bool;
+            default = true;
+            description = ''
+                  Set this to <literal>false</literal> to prevent NetworkManager
+                  from automaticly connecting to this network.
+                '';
+          };
+          priority = mkOption {
+            type = types.int;
+            default = 0;
+            description = ''
+                  When connecting automaticly networks with a higher priority are prevered 
+                  over networks with lower priority.
+                '';
+          };
         };
       };
     };
